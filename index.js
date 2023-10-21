748cc5f641aaf7ec9b3c2b8d559551fc7610de08
shoptorshi <shoptorshiria78@gmail.com>
2023-10-21 11:03:32 +0600

    user specific mycart added

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+
 app.use(express.json());
+const corsConfig ={
+  origin:"*",
+  credentials:true,
+  methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"]
+}
+app.use(cors(corsConfig));
 
 console.log(process.env.DB_USER, process.env.DB_PASS)
 
@@ -47,10 +53,22 @@ async function run() {
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     })
+     
     app.get('/myCart', async (req, res) => {
       const cursor = myCartCollection.find();
       const result = await cursor.toArray();
       res.send(result)
+      
+    })
+
+
+    app.get('/myCart/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = {uEmail: email}
+      const cursor = myCartCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+      console.log(email, result);
     })
 
     app.get("/myCart/:id", async(req, res)=>{
@@ -64,7 +82,7 @@ async function run() {
 
     app.delete("/myCart/:id", async(req, res)=>{
       const id = req.params.id;
-      const query = { _id: id };
+      const query = { _id: new ObjectId( id) };
       const result = await myCartCollection.deleteOne(query);
       res.send(result);
       
